12bc5fd7632224b5f80a086ff201946fbf14ff6b
FbrzRM <framosmesias@gmail.com>
2023-01-06 22:24:57 -0500

    Mock data snacks, menos texto

--- src/assets/ts/MOCK_DATA_SnackBar.ts
+++ src/assets/ts/MOCK_DATA_SnackBar.ts
@@ -10,7 +10,7 @@ export const SnackBar = [
         id_snack: 2,
         img: '../../../../../assets/img/snacks/popcorn.jfif',
         nombre: 'CANGUIL PEQUEÑO 85oz',
-        descripcion: '"¡Ven y disfruta de un delicioso y crocante Canguil saladito de 850 oz mientras ves tu película favorita en el cine! ¡Es el aperitivo perfecto para disfrutar durante el show!"',
+        descripcion: '"¡Ven y disfruta de un delicioso y crocante Canguil saladito de 850 oz mientras ves tu película favorita en el cine!"',
         precio: 2
     },
     {
@@ -24,14 +24,14 @@ export const SnackBar = [
         id_snack: 3,
         img: '../../../../../assets/img/snacks/nachos-gaseosa.jpg',
         nombre: 'Nachos con queso Y Gaseosa',
-        descripcion: 'eliciosas Hojuelas de maíz acompañadas de salsa de queso Cheddar y una refrescante gaseosa de 22oz en el cine! ¡Es el aperitivo perfecto para disfrutar durante el show!',
+        descripcion: 'eliciosas Hojuelas de maíz acompañadas de salsa de queso Cheddar y una refrescante gaseosa de 22oz en el cine!',
         precio: 5
     },
     {
         id_snack: 4,
         img: '../../../../../assets/img/snacks/hot-dog.jpg',
         nombre: 'Hot dog',
-        descripcion: '"Deliciosos Hot Dogs de Res o Pollo, siempre frescos y calientes en nuestro cine. ¡Un aperitivo perfecto durante el show! ¡Ven y prueba uno hoy!"',
+        descripcion: '"Deliciosos Hot Dogs de Res o Pollo, siempre frescos y calientes en nuestro cine. ¡Un aperitivo perfecto durante el show!"',
         precio: 4
     },
     {
