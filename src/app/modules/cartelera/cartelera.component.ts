adbf18d4ce5f8784ccf38c44a72a3e6529cba177
Acosta J <69605526+Sprogramdor@users.noreply.github.com>
2023-01-09 02:57:18 -0500

    a1

--- src/app/modules/cartelera/cartelera.component.ts
+++ src/app/modules/cartelera/cartelera.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {MatTabsModule} from '@angular/material/tabs';
+
+
+
+interface Tipos {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-cartelera',
   templateUrl: './cartelera.component.html',
 })
+
+
 export class CarteleraComponent {
 
-  constructor() { }
+  
+constructor() { 
+  
+      this.tform = new FormGroup({
+      tipopelicula: this.TiposControl
+    }); }
+
+ 
+    tform: FormGroup;
+ 
+  tipos: Tipos[] = [
+    {value: '0', viewValue: "Estrenos"},
+    {value: '1', viewValue: 'Preventas'},
+    {value: '2', viewValue: 'Disponibles'},
+    {value: '3', viewValue: 'Proxiamente'},
+  ];
+
+
+
+  TiposControl = new FormControl(this.tipos[2].viewValue);
+
+  
 
 
 }
