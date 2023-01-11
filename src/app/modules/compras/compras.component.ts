cf934eba8ac21bf8ed8d39189bef35a575a15974
Acosta J <69605526+Sprogramdor@users.noreply.github.com>
2023-01-11 15:53:28 -0500

    ok

--- src/app/modules/compras/compras.component.ts
+++ src/app/modules/compras/compras.component.ts
@@ -1,4 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from '@angular/router';
+import { PeliculasServices } from "src/app/shared/services/peliculas.service";
+import { Pelicula } from "src/app/shared/interfaces/pelicula.interface";
 
 @Component({
     selector: 'app-compras',
@@ -6,4 +9,27 @@ import { Component } from "@angular/core";
     styleUrls: ['./compras.component.css']
 })
 
-export class ComprasComponent{}
+export class ComprasComponent implements OnInit{
+
+    constructor(private route: ActivatedRoute, private pelis:PeliculasServices, ){
+      
+    }
+    
+    id_pelicula:any;
+    infopeli=null;
+
+    ngOnInit() {
+         this.id_pelicula=this.route.snapshot.paramMap.get("id");
+
+         for( let i of this.pelis.peliculas){
+            if(i.id_pelicula==this.id_pelicula){
+                this.infopeli
+            }
+
+         }
+    }
+
+    
+
+    
+}
