7da7a7be7e1765a2d0c9b9b751c3da4127f25f56
Acosta J <69605526+Sprogramdor@users.noreply.github.com>
2023-01-13 00:56:01 -0500

    form detalles

--- src/app/modules/registrar/registrar.component.ts
+++ src/app/modules/registrar/registrar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
-import { FormControl, Validators } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+
 
 @Component({
     selector :'app-registrar',
@@ -8,6 +9,28 @@ import { FormControl, Validators } from "@angular/forms";
 
 })
 
-export class RegistrarComponent{
-    emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+export class RegistrarComponent implements OnInit{
+    
+  
+        emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+        telf= new FormControl('',Validators.required);
+        nombres= new FormControl('',Validators.required);
+        apellidos= new FormControl('', Validators.required);
+        direccion= new FormControl('', Validators.required);
+        ciudad= new FormControl('', Validators.required);
+        cedula= new FormControl('', Validators.required);
+        password= new FormControl('', Validators.required);
+        passwordrepeat= new FormControl('', Validators.required);
+        username= new FormControl('', Validators.required);
+   
+
+  
+
+    ngOnInit(){
+       
+    }
+
+
+
+   
 }
