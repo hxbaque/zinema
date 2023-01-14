d4b0bfa9909408492095464e453adecb1132e537
Acosta J <69605526+Sprogramdor@users.noreply.github.com>
2023-01-14 17:31:46 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,7 @@ import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { ModulesModule } from "./modules/modules.module";
 import { AppRoutingModule } from './app-routing.module';
-<<<<<<< HEAD
-//import { PromocionesComponent } from './modules/promociones/promociones/promociones.component';
-import { PromDialogSecComponent } from './modules/promociones/prom-dialog-sec/prom-dialog-sec.component';
-import { PromDialogComponent } from './modules/promociones/prom-dialog/prom-dialog.component';
-=======
->>>>>>> parent of f0f9d1b (Promociones update)
+
 
 @NgModule({
   declarations: [
